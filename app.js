db3deee9dfde0ed9c49366d83f2984ca6e8afacb
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-04-05 21:50:10 +0200

    building app with JS

---
+++ app.js
@@ -0,0 +1,15 @@
+const buttonEl = document.querySelector('button');
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+
+const addGoal = () => {
+  let enteredValue = input.value;
+  if (enteredValue) {
+    const li = document.createElement('li');
+    li.textContent = enteredValue;
+    list.append(li);
+    input.value = '';
+  }
+};
+
+buttonEl.addEventListener('click', addGoal);
