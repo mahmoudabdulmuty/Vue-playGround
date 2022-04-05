9b2981922d1a452d399018ffd5d78aa29172d8ef
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-04-05 21:57:42 +0200

    Re-building the App with Vue

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const buttonEl = document.querySelector('button');
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
+const app = Vue.createApp({
+  data() {
+    return {
+      list: [],
+      enteredValue: '',
+    };
+  },
+  methods: {
+    addGoal() {
+      this.list.push(this.enteredValue);
+      this.enteredValue = '';
+    },
+  },
+});
 
-const addGoal = () => {
-  let enteredValue = input.value;
-  if (enteredValue) {
-    const li = document.createElement('li');
-    li.textContent = enteredValue;
-    list.append(li);
-    input.value = '';
-  }
-};
+app.mount('#app');
 
-buttonEl.addEventListener('click', addGoal);
+// const buttonEl = document.querySelector('button');
+// const list = document.querySelector('ul');
+// const input = document.querySelector('input');
+
+// const addGoal = () => {
+//   let enteredValue = input.value;
+//   if (enteredValue) {
+//     const li = document.createElement('li');
+//     li.textContent = enteredValue;
+//     list.append(li);
+//     input.value = '';
+//   }
+// };
+
+// buttonEl.addEventListener('click', addGoal);
