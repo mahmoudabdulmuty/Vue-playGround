bae61b3500d795e38b01ebedc74e90ec61338d12
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-04-08 14:38:14 +0200

    compueted prop. & watchers

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ const app = Vue.createApp({
       name: '',
     };
   },
-  methods: {
-    setName(event) {
-
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
     },
+  },
+  computed: {
+    fullName() {
+      console.log('running');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Abdulmuty';
+    },
+  },
+  methods: {
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
-      // this.counter--;
     },
   },
 });
