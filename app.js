7d752385a5e4a8247ca12d79d65ed663321004d4
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-04-13 05:19:49 +0200

    introducing components

--- app.js
+++ app.js
@@ -13,10 +13,34 @@ const app = Vue.createApp({
           email: 'julie@localhost.com',
         },
       ],
+    };
+  },
+});
+
+app.component('friend-contact',{
+  template: `
+  <li>
+    <h2>{{username}}</h2>
+    <button @click="toggleDetails">{{detailsValue}} Details</button>
+    <ul v-if="areDetailsVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{phone}}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{email}}
+      </li>
+    </ul>
+  </li>`,
+  data() {
+    return {
+      username: 'Manuel Lorenz',
+      phone: '01234 5678 991',
+      email: 'manuel@localhost.com',
       areDetailsVisible: false,
     };
   },
-
   methods: {
     toggleDetails() {
       this.areDetailsVisible = !this.areDetailsVisible;
